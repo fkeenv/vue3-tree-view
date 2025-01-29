f7ef1baf07786651added8ab4b9c8be7a56f4d5f
Keen <keenvergara@gmail.com>
2025-01-29 18:29:57 +0800

    chore: removed mounting to app

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
-import { createApp } from 'vue'
 import './assets/style.css'
-import App from './App.vue'
 import { defineCustomElement } from 'vue';
 import TreeView from './components/TreeView.vue';
 
 const VueTreeView = defineCustomElement(TreeView);
 customElements.define('vue-tree-view', VueTreeView);
-
-createApp(App).mount('#app')
 
 export { type TreeNode } from './types/tree-node';
 export default TreeView;
